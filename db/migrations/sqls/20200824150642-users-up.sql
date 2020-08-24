4bd930094f5881192082ae303123b63916ee1ba3
aa_makarov <aa_makarov@bingo-boom.com>
2020-08-24 18:35:04 +0300

    migrations

--- db/migrations/sqls/20200824150642-users-up.sql
+++ db/migrations/sqls/20200824150642-users-up.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+CREATE TABLE `users` (
+    user_id INTEGER(10) unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    status ENUM('enable', 'disable') DEFAULT 'enable',
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
